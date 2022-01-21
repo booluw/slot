fd39bc57d9153202de4f54dd2fc74cd6bf5d0c1d
= <=>
2022-01-21 06:06:33 +0100

    :tada: Configured service worker

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,21 @@
   </div>
 </template>
 
+<script>
+export default {
+  created() {
+    if (this.$workbox) {
+      this.$workbox.addEventListener("waiting", () => {
+        this.showUpgradeUI = true;
+      });
+    }
+  },
+
+  methods: {
+    async accept() {
+      this.showUpgradeUI = false
+      await this.$workbox.messageSW({ type: "SKIP_WAITING" });
+    }
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+
+import wb from "./registerServiceWorker";
+
+Vue.prototype.$workbox = wb;
+
 import VueSweetalert2 from 'vue-sweetalert2'
 import moment from 'moment'
 
